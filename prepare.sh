a38694d8027ed3982d1f555d8b7aef484df8df14
Chino <chino@rabbithouse.com>
2021-09-01 13:49:35 +0800

    add forgotten files

---
+++ prepare.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+CUBISM_SDK_VERSION="4-r.3"
+CUBISM_SDK_DIR="CubismSdkForNative-${CUBISM_SDK_VERSION}"
+CUBISM_SDK_ZIP="${CUBISM_SDK_DIR}.zip"
+CUBISM_URL="https://cubism.live2d.com/sdk-native/bin/${CUBISM_SDK_ZIP}"
+
+prepare_cubism_sdk() {
+	mkdir downloads
+	wget $CUBISM_URL -O downloads/${CUBISM_SDK_ZIP}
+	cd downloads
+	
+	# PWD=./downloads
+	unzip ${CUBISM_SDK_ZIP}
+	cd ${CUBISM_SDK_DIR}
+
+	# PWD=./downloads/${CUBISM_SDK_DIR}
+	mv ./Core/include/ ../../3rdparty/cubismcore/include
+	mv ./Core/dll/linux/x86_64/ ../../3rdparty/cubismcore/lib
+	cd ../../
+
+	# PWD=./
+	rm ./DeskFairy/src/Framework/CMakeLists.txt
+	rm -rf ./downloads/
+}
+
+
+prepare_cubism_sdk
---
+++ release.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+DESKFAIRY_RELEASE_VERSION="v0.3"
+DESKFAIRY_RELEASE_DIR="DeskFairy_${DESKFAIRY_RELEASE_VERSION}"
+DESKFAIRY_RELEASE_RAR="${DESKFAIRY_RELEASE_DIR}.rar"
+DESKFAIRY_RELEASE_URL="https://github.com/NeuraXmy/DeskFairy/releases/download/${DESKFAIRY_RELEASE_VERSION}/${DESKFAIRY_RELEASE_RAR}"
+
+
+download_resources() {
+	mkdir downloads
+	cd downloads
+	wget $DESKFAIRY_RELEASE_URL -O $DESKFAIRY_RELEASE_RAR
+	unar $DESKFAIRY_RELEASE_RAR
+	mv $DESKFAIRY_RELEASE_DIR/resources ../release/
+	cd ..
+	rm -rf downloads
+}
+
+mkdir release
+mv build/DeskFairy release/
+download_resources
